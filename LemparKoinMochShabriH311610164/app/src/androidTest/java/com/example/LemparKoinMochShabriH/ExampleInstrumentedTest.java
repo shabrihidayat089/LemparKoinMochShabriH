4cdae745d25ddee93ca46c692653f4fc33a11ad9
shabrihidayat089 <40760233+shabrihidayat089@users.noreply.github.com>
2021-04-03 16:45:51 +0700

    Update ExampleInstrumentedTest.java

--- LemparKoinMochShabriH311610164/app/src/androidTest/java/com/example/LemparKoinMochShabriH/ExampleInstrumentedTest.java
+++ LemparKoinMochShabriH311610164/app/src/androidTest/java/com/example/LemparKoinMochShabriH/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.lemparkoinreza311810033", appContext.getPackageName());
+        assertEquals("com.example.LemparKoinMochShabriH", appContext.getPackageName());
     }
 }
